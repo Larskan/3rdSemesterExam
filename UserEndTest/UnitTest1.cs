ba73fb4f86a330aba1fdffbbd600925714a326ac
PMPKN-dev <michal-janczak@outlook.com>
2022-11-17 10:32:36 +0100

    smthn

---
+++ UserEndTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserEndTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
