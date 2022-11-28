630c9eb6349d300a7372f538169685dc9c906e07
Patrick Schemel <patrick@schemel.dk>
2022-11-28 20:01:48 +0100

    Created Admin LogTool

--- Admin Client/ViewModel/ContentControlModels/AccountViewModel.cs
+++ Admin Client/ViewModel/ContentControlModels/AccountViewModel.cs
@@ -6,17 +6,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Admin_Client.ViewModel.ContentControlModels
 {
 	public class AccountViewModel : NotifyPropertyChangedHandler
 	{
 
-		#region Variables
-
-		#endregion
-
 		#region Properties
+
+		private string initials;
+
+		public string Initials
+		{
+			get { return initials; }
+			set { initials = value; NotifyPropertyChanged(); }
+		}
+
+		private string username;
+
+		public string Username
+		{
+			get { return username; }
+			set { username = value; NotifyPropertyChanged(); }
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; NotifyPropertyChanged(); }
+		}
+
+		private string phonenumber;
+
+		public string Phonenumber
+		{
+			get { return phonenumber; }
+			set { phonenumber = value; NotifyPropertyChanged(); }
+		}
+
 
 		#endregion
 
@@ -24,16 +54,26 @@ namespace Admin_Client.ViewModel.ContentControlModels
 
 		public AccountViewModel()
 		{
-
+			// Get Current USER
+			this.initials = "FL";
+			this.username = "FirstName LastName";
+			this.email = "First@Last.com";
+			this.phonenumber = "" + 42424242;
 		}
 
 		#endregion
 
 		#region Public Methods
 
-		#endregion
+		public void Edit()
+		{
+			// EDIT POPUP WITH CURRENT USER
+		}
 
-		#region Private Methods
+		public void LogTool()
+		{
+			MainWindowModelSingleton.Instance.StartPopoutLogTool();
+		}
 
 		#endregion
 
---
+++ Admin Client/ViewModel/WindowModels/Popout/PopoutLogToolWindowModel.cs
@@ -0,0 +1,67 @@
+﻿using Admin_Client.Model.Domain;
+using Admin_Client.PropertyChanged;
+using Admin_Client.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Admin_Client.ViewModel.WindowModels.Popout
+{
+	public class PopoutLogToolWindowModel : NotifyPropertyChangedHandler
+	{
+
+		#region Properties
+
+		private ObservableCollection<string> logFiles = new ObservableCollection<string>();
+
+		public ObservableCollection<string> LogFiles
+		{
+			get { return logFiles; }
+			set { logFiles = value; }
+		}
+
+		#endregion
+
+		#region Constructor
+
+		public PopoutLogToolWindowModel()
+		{
+			LogHandlerSingleton.Instance.WriteToLogFile(new Log("Open Popout LogToolWindow"));
+
+			List<string> localLogFiles = new List<string>();
+			foreach (var item in LogHandlerSingleton.Instance.GetLocalLogFiles())
+			{
+				localLogFiles.Add(item.ToString());
+			}
+
+			localLogFiles.Reverse();
+
+			foreach (var item in localLogFiles)
+			{
+				LogFiles.Add(item);
+			}
+
+			LogFiles[0] = LogFiles[0] + " ( Current )";
+
+			LogHandlerSingleton.Instance.WriteToLogFile(new Log(LogType.Success, "LogToolWindow is shown"));
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void OpenLogFile(DateTime dateTime)
+		{
+			LogHandlerSingleton.Instance.WriteToLogFile(new Log(LogType.UserAction, "LogTool Click --> " + dateTime.ToString()));
+			MainWindowModelSingleton.Instance.StartPopoutLog(dateTime);
+		}
+
+		#endregion
+
+	}
+}
