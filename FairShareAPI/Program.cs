7f1a43260addf05c88eefb69c5fcfc445a12e971
Patrick Schemel <Patrick@schemel.dk>
2022-11-30 12:23:15 +0100

    Fixed API Cors Problem

--- FairShareAPI/Program.cs
+++ FairShareAPI/Program.cs
@@ -15,22 +15,19 @@ builder.Services.AddDbContext<FairShareDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.AllowAnyOrigin();
-                      });
-});
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    }
+        )
+);
 
 var app = builder.Build();
 
-app.UseCors(builder => builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    );
+app.UseCors();
 
 app.UseSwagger();
 
