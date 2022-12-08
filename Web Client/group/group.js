5a81d559e49a3904dc645bae847219f0a6040296
PMPKN-dev <michal-janczak@outlook.com>
2022-12-08 12:48:15 +0100

    figured out the fetch api figured out basic json aiming to sync

--- Web Client/group/group.js
+++ Web Client/group/group.js
@@ -54,12 +54,39 @@ function PopulateTripTable()
     CreateTripTableElement('Trip To Spain','16.06.2012','$30.000,00');
     CreateTripTableElement('Moms birthday party','12.02.2015','$240.000,00');
     CreateTripTableElement('Cat food','01.01.1980','$-2.147.483.647,00');
+    TripSetter('tblTrips','1')
 
 }
 
 function PopulateMemberTable(){
-    CreateMemberTableElement('John');
-    CreateMemberTableElement('Mary');
-    CreateMemberTableElement('July');
+    loadMember('1'); // replace with a sort of for each loop that gets all of the users inside of a group 
 
+    for(i = 0; i<50;i++)
+    {
+        loadMember('1');
+    }
+}
+
+/** loads a member into the name table based on id by handing it over to NameSetter with the params tblUser and fldFirstname*/
+async function loadMember(id)
+{
+    NameSetter('tblUsers',id,'fldFirstName')    
+}
+
+//i have an issue with this, in the form that i cannot conveniently return the data from the query
+//I would like to do so as it would allow for firstName+lastName display for clarity 
+async function NameSetter(table, id, field)
+{
+    url = "https://localhost:7002/"+table+"/"+id
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) => CreateMemberTableElement(data[field]))
+}
+
+async function TripSetter(table, id)
+{
+    url = "https://localhost:7002/"+table+"/"+id
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) => CreateTripTableElement('Testing the water','no date',data['fldSum']))
 }
